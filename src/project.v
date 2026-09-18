5870f4ded2c74f0a501f204c06b96378855d12b7
anonymous <stefan.tufler@stud.ken.ch>
2026-09-18 14:41:18 +0200

    Add module declaration for tt_um_yourname_game

--- src/project.v
+++ src/project.v
@@ -1,1 +1,10 @@
-.
+module tt_um_yourname_game (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
